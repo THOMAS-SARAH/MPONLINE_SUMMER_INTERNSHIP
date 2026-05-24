706af8b75019a4cb7cfaa08934538fbb4c2e0feb
Sarah Thomas <167546833+THOMAS-SARAH@users.noreply.github.com>
2026-05-24 21:56:33 +0530

    Implement switch case for arithmetic operations
    Refactored code to implement a switch case for arithmetic operations and added a loop for user interaction.

--- WEEK01/IN_CLASS ASSIGNMENT/CODE03.cs
+++ WEEK01/IN_CLASS ASSIGNMENT/CODE03.cs
@@ -4,60 +4,7 @@ using System.Text;
 
 namespace proj1
 {
-    internal class ConditionalExample
-    {
-        //CLASS03--CONDITIONAL STATEMENTS-MAAM GIVEN CODE
-
-
-        /*public static void sarah1()
-        {
-            Console.Write("Enter first number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter second number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the third number: ");
-            int num3 = Convert.ToInt32(Console.ReadLine());
-
-            if (num1 >= num2 && num1 >= num3)
-            {
-                Console.WriteLine("The largest number is: " + num1);
-            }
-            else if (num2 >= num1 && num2 >= num3)
-            {
-                Console.WriteLine("The largest number is: " + num2);
-            }
-            else
-            {
-                Console.WriteLine("The largest number is: " + num3);
-            }
-
-        }*/
-        //CLASS03--IF ELSE STATEMENT-ASSIGNMENT QUESTION
-        //Accept character from user and check whether it is a letter, digit or special character.
-
-
-        /*public void check()
-        {
-            Console.Write("Enter a character:");
-            char c = Convert.ToChar(Console.ReadLine());
-
-            if (char.IsLetter(c))
-            {
-                Console.WriteLine("The character is a letter.");
-
-            }
-            else if (char.IsDigit(c))
-            {
-                Console.WriteLine("The character is a digit.");
-            }
-
-            else
-            {
-                Console.WriteLine("The character is a special character.");
-            }
-        }*/
-
-        //ASSIGNMENT QUESTION - USE SWITCH CSE TO SHOW ARITHMATIC OPERATORS and perform repeation based on user's choice
+     //ASSIGNMENT QUESTION - USE SWITCH CSE TO SHOW ARITHMATIC OPERATORS and perform repeation based on user's choice
 
         public static void func()
         {
@@ -90,12 +37,24 @@ namespace proj1
                 Console.Write("\nStart a new batch? (y/n): ");
                 continueProgram = Console.ReadLine().ToLower() == "y";
             }
-
-        }
-        
-        
-    }
 }
+
+using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConditionalExample.func();
+        }
+    }
+    
+    
+    
+    
+        
+        
+    
 
 
 
