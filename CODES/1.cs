e2f5f5e509a4c3339ba326284e94426fa97b8792
Sarah Thomas <167546833+THOMAS-SARAH@users.noreply.github.com>
2026-06-18 16:13:57 +0530

    Update 1.cs

--- CODES/1.cs
+++ CODES/1.cs
@@ -1,1 +1,47 @@
+// simple code using conditional statement to find the largest between the two numbers
 
+namespace proj1
+{
+    internal class ConditionalExample
+    {
+        //CLASS03--CONDITIONAL STATEMENTS-MAAM GIVEN CODE
+
+
+        public static void sarah1()
+        {
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the third number: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 >= num2 && num1 >= num3)
+            {
+                Console.WriteLine("The largest number is: " + num1);
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                Console.WriteLine("The largest number is: " + num2);
+            }
+            else
+            {
+                Console.WriteLine("The largest number is: " + num3);
+            }
+        }  
+    }
+} 
+
+
+//code for program.cs file
+
+namespace proj1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConditionalExample.sarah1();
+        }
+    }
+}
