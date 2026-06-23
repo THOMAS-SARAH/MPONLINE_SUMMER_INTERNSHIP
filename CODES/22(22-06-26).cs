72d1f9631de7565541b78e99c55ed86cd7d125f5
Sarah Thomas <167546833+THOMAS-SARAH@users.noreply.github.com>
2026-06-23 16:05:48 +0530

    Add number guessing game implementation
    Implement a number guessing game where the user guesses a randomly generated number between 1 and 100. The game provides feedback on whether the guess is too low, too high, or correct.

--- CODES/22(22-06-26).cs
+++ CODES/22(22-06-26).cs
@@ -1,1 +1,41 @@
+//Make a basic number guessing challenge against the computer
+
+using System;
+
+class NumberGuessing
+{
+    public static void Play()
+    {
+        Random random = new Random();
+        int number = random.Next(1, 101);
+        int guess;
+
+        Console.WriteLine("Guess a number between 1 and 100");
+
+        do
+        {
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < number)
+                Console.WriteLine("Too low!");
+            else if (guess > number)
+                Console.WriteLine("Too high!");
+            else
+                Console.WriteLine("Correct!");
+        }
+        while (guess != number);
+    }
+}
+
+// program class
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        NumberGuessing.Play();
+    }
+}
 
