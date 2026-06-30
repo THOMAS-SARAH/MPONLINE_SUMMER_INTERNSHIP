32f03a9cf9d42105e70d4953d1e395b26933d3d1
Sarah Thomas <167546833+THOMAS-SARAH@users.noreply.github.com>
2026-06-30 11:40:27 +0530

    Create Account.cs

---
+++ CODES/42(BankingMVC)/Model/Account.cs
@@ -0,0 +1,11 @@
+namespace BankingMVC.Models
+{
+    public class Account
+    {
+        public int AccountNumber { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public double Balance { get; set; }
+    }
+}
