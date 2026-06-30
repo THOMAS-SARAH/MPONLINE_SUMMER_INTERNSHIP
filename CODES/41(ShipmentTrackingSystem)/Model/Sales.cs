c9056aa5daaf518ace7464fafff6aeb127b2fea9
Sarah Thomas <167546833+THOMAS-SARAH@users.noreply.github.com>
2026-06-30 11:36:03 +0530

    Add Sales model class for tracking sales data

---
+++ CODES/41(ShipmentTrackingSystem)/Model/Sales.cs
@@ -0,0 +1,13 @@
+namespace ShipmentTrackingSystem.Models
+{
+    class Sales
+    {
+        public int SaleID { get; set; }
+
+        public string Salesman { get; set; }
+
+        public string Category { get; set; }
+
+        public double Amount { get; set; }
+    }
+}
