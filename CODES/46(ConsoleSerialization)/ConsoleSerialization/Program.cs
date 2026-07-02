d867f59a12290292d4115afb094ea78a4f8ca374
Sarah Thomas <167546833+THOMAS-SARAH@users.noreply.github.com>
2026-07-02 17:08:53 +0530

    Update Program.cs

--- CODES/46(ConsoleSerialization)/ConsoleSerialization/Program.cs
+++ CODES/46(ConsoleSerialization)/ConsoleSerialization/Program.cs
@@ -1,1 +1,42 @@
+using System;
+using System.Text.Json;
 
+namespace ConsoleSerialization
+{
+    internal class Program
+    {
+        static string json;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            StudentSerializing();
+            StudentDeSerializing();
+        }
+
+        static void StudentSerializing()
+        {
+            Student student = new Student
+            {
+                Name = "Smita",
+                Age = 23,
+                StudentId = 43
+            };
+
+            Console.WriteLine(student.GetHashCode());
+
+            json = JsonSerializer.Serialize(student);
+
+            Console.WriteLine(json);
+        }
+
+        static void StudentDeSerializing()
+        {
+            Student student = JsonSerializer.Deserialize<Student>(json);
+
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.GetHashCode());
+        }
+    }
+}
