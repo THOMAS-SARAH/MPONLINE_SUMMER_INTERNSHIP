6a80cbb4839328696566e617ccf3a76ac355797b
Sarah Thomas <167546833+THOMAS-SARAH@users.noreply.github.com>
2026-07-02 17:09:18 +0530

    Update Student.cs

--- CODES/46(ConsoleSerialization)/ConsoleSerialization/Student.cs
+++ CODES/46(ConsoleSerialization)/ConsoleSerialization/Student.cs
@@ -1,1 +1,16 @@
+namespace ConsoleSerialization
+{
+    internal class Student
+    {
+        public int StudentId { get; set; }
 
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string ToUpper()
+        {
+            return Name.ToUpper();
+        }
+    }
+}
